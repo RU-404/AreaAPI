cf177d2b4403629f8600dc93b6e9504bd841a201
ru-404 <ksy104701@daum.net>
2020-08-03 09:53:52 +0900

    add Events
    add Events
    
    if tou want to add events,
    use areaEvent class

---
+++ src/Ru/AreaAPI/event/deleteAreaEvent.php
@@ -0,0 +1,17 @@
+<?php
+
+
+namespace Ru\AreaAPI\event;
+
+
+use pocketmine\event\Cancellable;
+
+/**
+ * Class deleteAreaEvent
+ * @package Ru\AreaAPI\event
+ */
+
+class deleteAreaEvent extends areaEvent implements Cancellable
+{
+
+}
---
+++ src/Ru/AreaAPI/event/makeAreaEvent.php
@@ -0,0 +1,17 @@
+<?php
+
+
+namespace Ru\AreaAPI\event;
+
+
+use pocketmine\event\Cancellable;
+
+/**
+ * Class makeAreaEvent
+ * @package Ru\AreaAPI\event
+ */
+
+class makeAreaEvent extends areaEvent implements Cancellable
+{
+
+}
